0152f61111c825ffc38b72359aad0b1ffd3e9f44
Emanuel Zapata <emanuelzapata@ymail.com>
2021-03-15 00:28:40 -0500

    mx units on a truck completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import maximum_units_on_a_truck as muoat
 
 def main():
-    #print("main")
-    max_units = muoat.MaximumUnitsOnATruck("name")
-    print(max_units.testval)
+    #temp = muoat.MaximumUnitsOnATruck([[1,3],[2,2],[3,1]],4)
+    temp = muoat.MaximumUnitsOnATruck([[5,10],[2,5],[4,7],[3,9]], truckSize = 10)
+    print(temp.calculate_units())
 
 if __name__=="__main__":
     main()
--- maximum_units_on_a_truck.py
+++ maximum_units_on_a_truck.py
@@ -1,3 +1,18 @@
+from typing import List
+
 class MaximumUnitsOnATruck:
-    def __init__(self,testval):
-        self.testval = testval
+    def __init__(self, boxTypes: List[List[int]], truckSize: int):
+        self.boxTypes = boxTypes
+        self.truckSize = truckSize        
+    def calculate_units(self)->int:
+        temp_list = sorted(self.boxTypes, key=lambda x: x[1], reverse=True)
+        sum = 0
+        for box in temp_list:
+            if(self.truckSize == 0):
+                break
+            if(box[0] > self.truckSize):
+                sum = sum + (self.truckSize*box[1])
+                break
+            sum = sum + (box[0] * box[1])
+            self.truckSize = self.truckSize - box[0]
+        return sum
